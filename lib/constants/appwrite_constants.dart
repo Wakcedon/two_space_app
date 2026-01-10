c350ddc26bc736c311d6dcd08823f6748732f2d8
Wakcedon <vaksedon@gmail.com>
2026-01-11 02:54:41 +0700

    Delete lib/constants/appwrite_constants.dart

--- lib/constants/appwrite_constants.dart
+++
@@ -1,8 +0,0 @@
-import '../config/environment.dart';
-
-class AppwriteConstants {
-  static String projectId = Environment.appwriteProjectId;
-  static String endpoint = Environment.appwritePublicEndpoint;
-  static String apiKey = Environment.appwriteApiKey;
-  static String databaseId = Environment.appwriteDatabaseId;
-}
